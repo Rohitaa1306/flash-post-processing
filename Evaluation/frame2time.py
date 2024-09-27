c9f8c27c043b91a6f5015c23f6a758e2bfb7d220
Ravikumar <u255769@bcm.edu>
2024-09-27 14:11:35 -0500

    Final check

--- Evaluation/frame2time.py
+++ Evaluation/frame2time.py
@@ -1,17 +1,19 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
-start_time, fps = "12:54:08", 30
+start_time = "2023-09-25 12:51:17"
+fps = 30
 file_path = 'C:\\Users\\u255769\\Downloads\\Evaluation\\606_tc_gaze_epoch_5s_30fps.txt'
 
 data = pd.read_csv(file_path, sep=' ', header=None, usecols=[5, 10], names=['Frame', 'Category'])
 data['Category'] = data['Category'].map({'Gaze': 1, 'No-Gaze': 0, 'Uncertain': 0, 'Out-of-Frame': 0})
-data['Timestamp'] = data['Frame'].apply(lambda x: (datetime.strptime(start_time, "%H:%M:%S") + timedelta(seconds=x/fps)))
+data['Timestamp'] = pd.to_datetime(start_time) + pd.to_timedelta(data['Frame'] // fps, unit='s')
 data = data.drop_duplicates(subset='Timestamp')
+data = data.set_index('Timestamp')
 
-gaze_data = data[data['Category'] == 1].sort_values(by='Timestamp')
-gaze_data['Duration'] = gaze_data['Timestamp'].diff().dt.total_seconds()
+new_time_range = pd.date_range(start=start_time, end=data.index.max(), freq='2S')
+result = data.reindex(new_time_range, method='ffill').reset_index()
+count_category_1 = result[result['Category'] == 1.0].shape[0]
 
-total_time_seconds = gaze_data['Duration'].sum()
-
-print(f"Total time in seconds where Category is 1: {total_time_seconds}")
+print(result)
+print(f"Number of rows with Category 1.0: {count_category_1}")
